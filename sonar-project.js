899a2e5b8560937150ee3610d6087054eae4f458
Mahesh <devopssmart493@gmail.com>
2023-05-06 07:50:03 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,8 +6,9 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'mahesh@123',
+	    'sonar.login': 'squ_007829a6ff33d9b20f4280fcdd3c09d8eada3587',
+	    //'sonar.login':'admin'
+	    //'sonar.password': 'mahesh@123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
